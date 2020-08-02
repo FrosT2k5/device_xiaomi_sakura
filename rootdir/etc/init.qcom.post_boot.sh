f0f718047c025c949435ac936321346f27bc3d37
ZèD <imzihad@gmail.com>
2020-08-02 05:54:08 +0000

    sakura: Initialize Revvz configs on boot
    Signed-off-by: ZèD <imzihad@gmail.com>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -4339,3 +4339,42 @@ fi
 misc_link=$(ls -l /dev/block/bootdevice/by-name/misc)
 real_path=${misc_link##*>}
 setprop persist.vendor.mmi.misc_dev_path $real_path
+
+### Revvz configurations
+
+#Schedutil changes
+echo 1500 > /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us
+echo 1000 > /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
+echo 1401600 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_freq
+echo 85 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_load
+
+#GPU(KGSL) changes
+echo 6 > /sys/devices/platform/soc/1c00000.qcom,kgsl-3d0/kgsl/kgsl-3d0/default_pwrlevel
+
+#VM Changes
+echo 40 > /proc/sys/vm/dirty_ratio
+echo 60 > /proc/sys/vm/vfs_cache_pressure
+echo 85 > /proc/sys/vm/swappiness
+echo 3600 > /proc/sys/vm/stat_interval
+
+#Misc changes
+sysctl -w net.ipv4.tcp_congestion_control=westwood
+
+#Uclamp-related changes
+sysctl -w kernel.sched_rt_default_util_clamp_min=500
+sysctl -w kernel.sched_util_clamp_min=128
+
+echo max > /dev/cpuset/top-app/uclamp.max
+echo 50 > /dev/cpuset/top-app/uclamp.min
+echo 1   > /dev/cpuset/top-app/uclamp.latency_sensitive 
+
+echo 90 > /dev/cpuset/background/uclamp.max
+echo 40 > /dev/cpuset/background/uclamp.min
+echo 0  > /dev/cpuset/background/uclamp.latency_sensitive
+
+echo 40 > /dev/cpuset/foreground/uclamp.max
+echo 0 > /dev/cpuset/foreground/uclamp.min
+echo 1  > /dev/cpuset/foreground/uclamp.latency_sensitive
+
+echo 30 > /dev/cpuset/system-background/uclamp.max
+echo 20 > /dev/cpuset/camera-daemon/uclamp.max

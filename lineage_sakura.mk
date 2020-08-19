c929cb8185fb6e95ccb86256d67132df5e576829
FrosT2k5 <androygaming@gmail.com>
2020-08-19 10:55:32 +0530

    sakura: Define IS_PHONE true
    This adds is required to include phone and messaging app to the build

--- lineage_sakura.mk
+++ lineage_sakura.mk
@@ -40,3 +40,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+
+#set phone true
+IS_PHONE := true

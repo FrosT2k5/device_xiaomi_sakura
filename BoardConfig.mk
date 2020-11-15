74d9607fee184e6012597b2e035789fe548b46e5
FrosT2k5 <androygaming@gmail.com>
2020-11-15 12:27:41 +0530

    sakura: Ignore Neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,6 +199,7 @@ VENDOR_SECURITY_PATCH := 2020-02-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Sepolicy
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy

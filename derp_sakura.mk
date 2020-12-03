240366ed963e45b6aaf5c54a0bd21aabf95ab124
FrosT2k5 <androygaming@gmail.com>
2020-12-03 21:02:18 +0530

    derp_sakura.mk: Upstream to DerpFest R
    Changing vendor path and nuking old variables

--- derp_sakura.mk
+++ derp_sakura.mk
@@ -25,7 +25,7 @@ $(call inherit-product, vendor/MiuiCamera/config.mk)
 $(call inherit-product, device/xiaomi/sakura/device.mk)
 
 # Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sakura
@@ -44,9 +44,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
 
-#DerpFest stuff
-IS_PHONE := true
-TARGET_GAPPS_ARCH += arm64
 TARGET_BOOT_ANIMATION_RES += 1080
 
 #Official

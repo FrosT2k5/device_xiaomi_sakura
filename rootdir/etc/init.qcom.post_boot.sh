5f47930bbfa0bb2f03f384d71ec7f037f5b1c043
FrosT2k5 <androygaming@gmail.com>
2021-03-19 00:18:34 +0530

    sakura: switch to sleepy kernel

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -4340,53 +4340,125 @@ misc_link=$(ls -l /dev/block/bootdevice/by-name/misc)
 real_path=${misc_link##*>}
 setprop persist.vendor.mmi.misc_dev_path $real_path
 
-### Revvz configurations
+#Default settings
 
-#Schedutil changes
-echo 1500 > /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us
-echo 1000 > /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
-echo 1401600 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_freq
-echo 85 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_load
+write() {
+	chmod +w "$1" 2> /dev/null
 
-#GPU(KGSL) changes
-echo 6 > /sys/devices/platform/soc/1c00000.qcom,kgsl-3d0/kgsl/kgsl-3d0/default_pwrlevel
+	echo "$2" > "$1" 2> /dev/null
+}
 
-#VM Changes
-echo 40 > /proc/sys/vm/dirty_ratio
-echo 60 > /proc/sys/vm/vfs_cache_pressure
-echo 85 > /proc/sys/vm/swappiness
-echo 3600 > /proc/sys/vm/stat_interval
+# Duration in nanoseconds of one scheduling period
+SCHED_PERIOD="$((1 * 1000 * 1000))"
 
-#Misc changes
-sysctl -w net.ipv4.tcp_congestion_control=westwood
+# How many tasks should we have at a maximum in one scheduling period
+SCHED_TASKS="10"
 
-#Uclamp changes (reduces jitter,apparently)
+# Limit max perf event processing time to this much CPU usage
+write /proc/sys/kernel/perf_cpu_time_max_percent 3
 
-#system-wide
-sysctl -w kernel.sched_rt_default_util_clamp_min=500
-sysctl -w kernel.sched_util_clamp_min=128
+# Group tasks for less stutter but less throughput
+write /proc/sys/kernel/sched_autogroup_enabled 1
 
-#top-app
-echo max > /dev/cpuset/top-app/uclamp.max
-echo 85 > /dev/cpuset/top-app/uclamp.min
-echo 1   > /dev/cpuset/top-app/uclamp.latency_sensitive 
+# Execute child process before parent after fork
+# Better to set this to 0
+write /proc/sys/kernel/sched_child_runs_first 1
 
-#foreground
-echo 50 > /dev/cpuset/foreground/uclamp.max
-echo 20 > /dev/cpuset/foreground/uclamp.min
-echo 1  > /dev/cpuset/foreground/uclamp.latency_sensitive
+# Preliminary requirement for the following values
+write /proc/sys/kernel/sched_tunable_scaling 0
 
-#background
-echo max > /dev/cpuset/background/uclamp.max
-echo 20 > /dev/cpuset/background/uclamp.min
-echo 0  > /dev/cpuset/background/uclamp.latency_sensitive
+# Reduce the maximum scheduling period for lower latency
+write /proc/sys/kernel/sched_latency_ns "$SCHED_PERIOD"
 
-#system-background
-echo 50 > /dev/cpuset/system-background/uclamp.max
-echo 10 > /dev/cpuset/system-background/uclamp.min
-echo 0  > /dev/cpuset/system-background/uclamp.latency_sensitive
+# Schedule this ratio of tasks in the guarenteed sched period
+write /proc/sys/kernel/sched_min_granularity_ns "$((SCHED_PERIOD / SCHED_TASKS))"
 
-#camera-daemon
-echo 20 > /dev/cpuset/camera-daemon/uclamp.max
-echo 0 > /dev/cpuset/camera-daemon/uclamp.min
-echo 0  > /dev/cpuset/camera-daemon/uclamp.latency_sensitive
+# Require preeptive tasks to surpass half of a sched period in vmruntime
+write /proc/sys/kernel/sched_wakeup_granularity_ns "$((SCHED_PERIOD / 2))"
+
+# Reduce the frequency of task migrations
+write /proc/sys/kernel/sched_migration_cost_ns 5000000
+
+# Improve real time latencies by reducing the scheduler migration time
+write /proc/sys/kernel/sched_nr_migrate 4
+
+# Disable scheduler statistics to reduce overhead
+write /proc/sys/kernel/sched_schedstats 0
+
+# Disable unnecessary printk logging
+write /proc/sys/kernel/printk_devkmsg off
+
+# Start non-blocking writeback later
+write /proc/sys/vm/dirty_background_ratio 3
+
+# Start blocking writeback later
+write /proc/sys/vm/dirty_ratio 30
+
+# Require dirty memory to stay in memory for longer
+write /proc/sys/vm/dirty_expire_centisecs 3000
+
+# Run the dirty memory flusher threads less often
+write /proc/sys/vm/dirty_writeback_centisecs 3000
+
+# Disable read-ahead for swap devices
+write /proc/sys/vm/page-cluster 0
+
+# Update /proc/stat less often to reduce jitter
+write /proc/sys/vm/stat_interval 10
+
+# Swap to the swap device at a fair rate
+write /proc/sys/vm/swappiness 100
+
+# Prioritize page cache over simple file structure nodes
+write /proc/sys/vm/vfs_cache_pressure 200
+
+# Enable Explicit Congestion Control
+write /proc/sys/net/ipv4/tcp_ecn 1
+
+# Enable fast socket open for receiver and sender
+write /proc/sys/net/ipv4/tcp_fastopen 3
+
+# Disable SYN cookies
+write /proc/sys/net/ipv4/tcp_syncookies 0
+
+if [[ -f "/sys/kernel/debug/sched_features" ]]
+then
+	# Consider scheduling tasks that are eager to run
+	write /sys/kernel/debug/sched_features NEXT_BUDDY
+
+	# Some sources report large latency spikes during large migrations
+	write /sys/kernel/debug/sched_features NO_TTWU_QUEUE
+fi
+
+for queue in /sys/block/*/queue
+do
+	# Choose the first governor available
+	avail_scheds="$(cat "$queue/scheduler")"
+	
+	#This will be slow, since we also check for 
+	#schedulers currently unsupported by kernel,
+	#which is of no use.
+   for sched in cfq noop none #kyber bfq mq-deadline none
+	do
+		if [[ "$avail_scheds" == *"$sched"* ]]
+		then
+			write "$queue/scheduler" "$sched"
+			break
+		fi
+    done
+
+	# Do not use I/O as a source of randomness
+	write "$queue/add_random" 0
+
+	# Disable I/O statistics accounting
+	write "$queue/iostats" 0
+
+	# Reduce heuristic read-ahead in exchange for I/O latency
+	write "$queue/read_ahead_kb" 32
+
+	# Reduce the maximum number of I/O requests in exchange for latency
+	write "$queue/nr_requests" 32
+done
+
+#Default settings END
+
